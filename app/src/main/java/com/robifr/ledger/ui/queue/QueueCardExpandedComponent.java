c7fa272e09dc56502726f253075c291eec0f56e3
robi <robifr@users.noreply.github.com>
2024-07-09 16:48:33 +0700

    update: redesign orders table inside queue card with more flat looks
    includes:
    - add discount information on each product order in the table.

--- app/src/main/java/com/robifr/ledger/ui/queue/QueueCardExpandedComponent.java
+++ app/src/main/java/com/robifr/ledger/ui/queue/QueueCardExpandedComponent.java
@@ -21,16 +21,15 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TableRow;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import com.google.android.material.shape.ShapeAppearanceModel;
 import com.robifr.ledger.R;
 import com.robifr.ledger.data.model.CustomerModel;
 import com.robifr.ledger.data.model.ProductOrderModel;
 import com.robifr.ledger.data.model.QueueModel;
 import com.robifr.ledger.databinding.QueueCardWideExpandedBinding;
+import com.robifr.ledger.databinding.QueueCardWideExpandedOrderBinding;
 import com.robifr.ledger.databinding.QueueCardWideExpandedOrderDataBinding;
 import com.robifr.ledger.util.CurrencyFormat;
 import java.math.BigDecimal;
@@ -43,11 +42,13 @@ import java.util.Objects;
 public class QueueCardExpandedComponent {
   @NonNull private final Context _context;
   @NonNull private final QueueCardWideExpandedBinding _binding;
+  @NonNull private final QueueCardWideExpandedOrderBinding _productOrderBinding;
 
   public QueueCardExpandedComponent(
       @NonNull Context context, @NonNull QueueCardWideExpandedBinding binding) {
     this._context = Objects.requireNonNull(context);
     this._binding = Objects.requireNonNull(binding);
+    this._productOrderBinding = QueueCardWideExpandedOrderBinding.bind(this._binding.getRoot());
 
     this._binding.customerImage.shapeableImage.setShapeAppearanceModel(
         ShapeAppearanceModel.builder(
@@ -80,20 +81,17 @@ public class QueueCardExpandedComponent {
     this._binding.statusChip.setChipBackgroundColor(null);
     this._binding.coloredSideline.setBackgroundColor(0);
 
-    this._binding.productOrderTotalRow.totalDiscount.setText(null);
-    this._binding.productOrderTotalRow.grandTotalPrice.setText(null);
-    this._binding.productOrderTotalRow.customerBalanceTitle.setText(null);
-    this._binding.productOrderTotalRow.customerBalance.setText(null);
-    this._binding.productOrderTotalRow.customerDebtTitle.setText(null);
-    this._binding.productOrderTotalRow.customerDebt.setText(null);
-    this._binding.productOrderTotalRow.customerDebt.setTextColor(0);
-    // Remove all data rows type in table except for both
-    // title — index 0 — and total — last index — row type.
-    this._binding.productOrderTable.removeViews(
-        1, this._binding.productOrderTable.getChildCount() - 2);
-
     this._binding.customerImage.text.setText(null);
     this._binding.customerName.setText(null);
+
+    this._productOrderBinding.table.removeAllViews();
+    this._productOrderBinding.totalDiscount.setText(null);
+    this._productOrderBinding.grandTotalPrice.setText(null);
+    this._productOrderBinding.customerBalanceTitle.setText(null);
+    this._productOrderBinding.customerBalance.setText(null);
+    this._productOrderBinding.customerDebtTitle.setText(null);
+    this._productOrderBinding.customerDebt.setText(null);
+    this._productOrderBinding.customerDebt.setTextColor(0);
   }
 
   private void _setId(@Nullable Long id) {
@@ -131,14 +129,14 @@ public class QueueCardExpandedComponent {
   private void _setTotalDiscount(@NonNull BigDecimal totalDiscount) {
     Objects.requireNonNull(totalDiscount);
 
-    this._binding.productOrderTotalRow.totalDiscount.setText(
+    this._productOrderBinding.totalDiscount.setText(
         CurrencyFormat.format(totalDiscount, "id", "ID"));
   }
 
   private void _setGrandTotalPrice(@NonNull BigDecimal grandTotalPrice) {
     Objects.requireNonNull(grandTotalPrice);
 
-    this._binding.productOrderTotalRow.grandTotalPrice.setText(
+    this._productOrderBinding.grandTotalPrice.setText(
         CurrencyFormat.format(grandTotalPrice, "id", "ID"));
   }
 
@@ -147,10 +145,10 @@ public class QueueCardExpandedComponent {
       this._binding.customerImage.text.setText(null);
       this._binding.customerName.setText(this._context.getString(R.string.symbol_notavailable));
       this._binding.customerName.setEnabled(false);
-      this._binding.productOrderTotalRow.customerBalanceTitle.setVisibility(View.GONE);
-      this._binding.productOrderTotalRow.customerBalance.setVisibility(View.GONE);
-      this._binding.productOrderTotalRow.customerDebtTitle.setVisibility(View.GONE);
-      this._binding.productOrderTotalRow.customerDebt.setVisibility(View.GONE);
+      this._productOrderBinding.customerBalanceTitle.setVisibility(View.GONE);
+      this._productOrderBinding.customerBalance.setVisibility(View.GONE);
+      this._productOrderBinding.customerDebtTitle.setVisibility(View.GONE);
+      this._productOrderBinding.customerDebt.setVisibility(View.GONE);
       return;
     }
 
@@ -167,33 +165,26 @@ public class QueueCardExpandedComponent {
     this._binding.customerName.setText(customer.name());
     this._binding.customerName.setEnabled(true);
 
-    // Displaying customer data on the product orders table, total typed row.
-    this._binding.productOrderTotalRow.customerBalanceTitle.setText(
+    // Displaying customer data on the product orders detail.
+    this._productOrderBinding.customerBalanceTitle.setText(
         this._context.getString(R.string.productordercard_customerbalance_title, croppedName));
-    this._binding.productOrderTotalRow.customerBalanceTitle.setVisibility(View.VISIBLE);
-    this._binding.productOrderTotalRow.customerBalance.setText(
+    this._productOrderBinding.customerBalanceTitle.setVisibility(View.VISIBLE);
+    this._productOrderBinding.customerBalance.setText(
         CurrencyFormat.format(BigDecimal.valueOf(customer.balance()), "id", "ID"));
-    this._binding.productOrderTotalRow.customerBalance.setVisibility(View.VISIBLE);
-    this._binding.productOrderTotalRow.customerDebtTitle.setText(
+    this._productOrderBinding.customerBalance.setVisibility(View.VISIBLE);
+    this._productOrderBinding.customerDebtTitle.setText(
         this._context.getString(R.string.productordercard_customerdebt_title, croppedName));
-    this._binding.productOrderTotalRow.customerDebtTitle.setVisibility(View.VISIBLE);
-    this._binding.productOrderTotalRow.customerDebt.setText(
+    this._productOrderBinding.customerDebtTitle.setVisibility(View.VISIBLE);
+    this._productOrderBinding.customerDebt.setText(
         CurrencyFormat.format(customer.debt(), "id", "ID"));
-    this._binding.productOrderTotalRow.customerDebt.setTextColor(debtTextColor);
-    this._binding.productOrderTotalRow.customerDebt.setVisibility(View.VISIBLE);
+    this._productOrderBinding.customerDebt.setTextColor(debtTextColor);
+    this._productOrderBinding.customerDebt.setVisibility(View.VISIBLE);
   }
 
   private void _setProductOrders(@NonNull List<ProductOrderModel> productOrders) {
     Objects.requireNonNull(productOrders);
 
-    // Obtain both title and total typed row before clearing all the rows.
-    final TableRow titleRow =
-        this._binding.productOrderTable.findViewById(R.id.productOrderHeaderRow);
-    final ConstraintLayout totalRow =
-        this._binding.productOrderTable.findViewById(R.id.productOrderTotalRow);
-
-    // Clear everything to ensure proper clean-up.
-    this._binding.productOrderTable.removeAllViews();
+    this._productOrderBinding.table.removeAllViews();
 
     for (ProductOrderModel productOrder : productOrders) {
       final QueueCardWideExpandedOrderDataBinding dataRowBinding =
@@ -210,21 +201,21 @@ public class QueueCardExpandedComponent {
           isProductPriceExists
               ? CurrencyFormat.format(BigDecimal.valueOf(productOrder.productPrice()), "id", "ID")
               : this._context.getString(R.string.symbol_notavailable);
-      final String quantity =
-          CurrencyFormat.format(BigDecimal.valueOf(productOrder.quantity()), "id", "ID", "");
-      final String totalPrice = CurrencyFormat.format(productOrder.totalPrice(), "id", "ID");
+      final int discountVisibility =
+          productOrder.discountPercent().compareTo(BigDecimal.ZERO) == 0 ? View.GONE : View.VISIBLE;
 
       dataRowBinding.productName.setText(productName);
       dataRowBinding.productName.setEnabled(isProductExists);
       dataRowBinding.productPrice.setText(productPrice);
-      dataRowBinding.productPrice.setEnabled(isProductExists);
-      dataRowBinding.quantity.setText(quantity);
-      dataRowBinding.totalPrice.setText(totalPrice);
-      this._binding.productOrderTable.addView(dataRowBinding.getRoot());
+      dataRowBinding.quantity.setText(
+          CurrencyFormat.format(BigDecimal.valueOf(productOrder.quantity()), "id", "ID", ""));
+      dataRowBinding.totalPrice.setText(
+          CurrencyFormat.format(productOrder.totalPrice(), "id", "ID"));
+      dataRowBinding.discount.setText(
+          this._context.getString(
+              R.string.productordercard_discount_title, productOrder.discountPercent()));
+      dataRowBinding.discount.setVisibility(discountVisibility);
+      this._productOrderBinding.table.addView(dataRowBinding.getRoot());
     }
-
-    // Add them back.
-    this._binding.productOrderTable.addView(titleRow, 0);
-    this._binding.productOrderTable.addView(totalRow);
   }
 }
